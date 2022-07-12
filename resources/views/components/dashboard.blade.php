63afaf9a752ecb3f3d325b5351e3e21573206421
andydevs69420 <redondophilippandrew@gmail.com>
2022-07-12 22:46:50 +0800

    pre finished dashboard

---
+++ resources/views/components/dashboard.blade.php
@@ -0,0 +1,102 @@
+
+
+<section id="{{ $attributes->get("id")?$attributes->get("id") : "dashboard-page"  }}" class="d-block w-100 h-100">
+    <div class="container-lg">
+        <div class="row">
+            <div class="col-12 col-sm-6 col-md-6 col-lg-4">
+                <div class="container-fluid my-2 px-0">
+                    <div class="row">
+                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                            {{-- TILE 0 --}}
+                            <div class="d-block dashboard__tile-small bg-white mb-0 mb-sm-3 mb-md-0 shadow">
+                            </div>
+                        </div>
+                        <div class="col-6 col-sm-12 col-md-6 col-lg-6">
+                            {{-- TILE 1 --}}
+                            <div class="d-block dashboard__tile-small bg-white shadow">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-12 col-lg-4 order-first order-lg-last">
+                <div class="container-fluid position-relative my-2 px-0">
+                    <div id="dashboard__tile-center" class="d-flex flex-row flex-nowrap mx-auto mb-4 px-3 py-5 bg-primary text-light shadow">
+                        <div class="d-block w-50 text-center">
+                            <span class="d-block h3">{{ __("0") }}</span>
+                            <span class="d-inline-block">{{ __("unavailable") }}</span>
+                        </div>
+                        <div class="vr bg-white"></div>
+                        <div class="d-block w-50 text-center">
+                            <span class="d-block h3">{{ __("0") }}</span>
+                            <span>{{ __("available") }}</span>
+                        </div>
+                    </div>
+                    <span id="dashboard_center-button-1" class="d-block mx-auto position-relative">
+                        <span id="dashboard__center-button-icon"
+                        class="d-flex flex-nowrap
+                               align-items-center
+                               justify-content-center
+                               position-absolute
+                               rounded-circle bg-primary text-light">
+                            <i class="fa-solid fa-car-rear"></i>
+                        </span>
+                    </span>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 order-last">
+                <div class="container-fluid my-2 px-0">
+                    <div class="row">
+                        <div class="col-6 col-md-6 col-lg-6">
+                            {{-- TILE 2 --}}
+                            <div class="d-block dashboard__tile-small mb-0 mb-sm-3 mb-md-0 shadow">
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-6 col-lg-6">
+                            {{-- TILE 3 --}}
+                            <div class="d-block dashboard__tile-small shadow">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-12 col-md-8">
+                {{-- TILE 0 --}}
+                <div class="d-block dashboard__tile-large my-2 shadow"></div>
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="card dashboard__card-tile border-0 my-2 shadow">
+                    <img class="card-img-top shadow" src="https://149366088.v2.pressablecdn.com/wp-content/uploads/2022/03/jammy-jellyfish-wallpaper.jpg" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title position-relative mb-0">
+                            <i class="fa-solid fa-crown dashboard__card-tile-icon text-warning" style="text-shadow: 2px 3px rgba(0,0,0,0.4)"></i>
+                            {{ __("Plan") }}
+                            <small class="small dashboard__card-plan rounded text-light bg-warning">{{ __("Pro") }}</small>
+                        </h5>
+                        <small class="card-subtitle small text-muted">{{ __("Your current subscription") }}</small>
+
+                        <div id="dashboard__card-more-info" class="collapse">
+                            <p class="card-text">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus recusandae magni dolorem dicta labore? Repellendus odit dolore cupiditate maxime officia?
+                            </p>
+                        </div>
+
+                        <div class="container-fluid mt-5 text-center">
+                            <span class="d-inline-block w-auto shadow rounded-pill">
+                                <button class="btn btn-sm btn-success px-3 rounded-pill" data-bs-toggle="collapse" data-bs-target="#dashboard__card-more-info" aria-expanded="false" aria-controls="dashboard__card-more-info">
+                                    {{ __("Read more") }}
+                                </button>
+                            </span>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -6,13 +6,15 @@
 @section("stylesheet")
     {{-- page styling css --}}
     <link rel="stylesheet" href="{{ variant(asset("css/main/default/main.css"), asset("css/main/default/main.css")) }}">
+    {{-- dashboard styling css --}}
+    <link rel="stylesheet" href="{{ variant(asset("css/components/dashboard/default/dashboard.css"), asset("css/main/default/main.css")) }}">
     {{-- variant selector css --}}
     <link rel="stylesheet" href="{{ asset("css/components/local-variant-selector/local-variant-selector.css") }}">
 @stop
 
 {{-- body --}}
 @section("app-body")
-    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+    <nav id="main__navbar" class="navbar navbar-expand-sm navbar-light bg-light">
         <div class="container-fluid">
 
             <a href="{{ url("/geocarsapp") }}" class="d-none d-sm-inline-block position-absolute navbar-brand">
@@ -21,7 +23,7 @@
 
             <ul id="main__link-list" class="navbar-nav flex-row justify-content-around mx-auto">
                 <li class="nav-item">
-                    <a href="#" class="nav-link active">
+                    <a href="#dashboard-page" class="nav-link">
                         <i class="d-inline d-sm-none fa-solid fa-chart-column"></i> <span class="d-none d-sm-inline small">{{ __("Dashboard") }}</span>
                     </a>
                 </li>
@@ -43,6 +45,9 @@
             </ul>
         </div>
     </nav>
+    <div id="main__content-area" class="container-fluid px-0 h-100" data-bs-spy="scroll" data-bs-target="#main__navbar">
+        <x-dashboard></x-dashboard>
+    </div>
 @stop
 
 {{-- js --}}
